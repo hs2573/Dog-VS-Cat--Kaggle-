eb7130937a1bbc61a20add175cf8a21d838a2cda
yi-hsuan,cheng <32381008+hs2573@users.noreply.github.com>
2017-10-21 09:36:44 +1100

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,212 @@
+# Convolutional Neural Network
+# Part 1 - Building the CNN
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+import os
+import matplotlib.pyplot as plt
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+
+test1_set = test_datagen.flow_from_directory('dataset/single_prediction',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+
+history = classifier.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 2000)
+
+classifier.save("classifier.model")
+
+# Part 3 - Making new predictions
+from keras.models import load_model
+
+classifier = load_model("classifier.model")
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_2.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+    
+predictions = np.round(classifier.predict(training_set))
+# Print result!
+import glob
+from PIL import Image
+import pandas as pd
+import os
+images=glob.glob("/home/yi-hsuancheng/Desktop/DogCat/test2/test/*.jpg")
+sub = pd.DataFrame()
+i= 0
+result = np.zeros(12501)
+dog = np.zeros(12500)
+cat = np.zeros(12500)
+imid = pd.DataFrame()
+while(i<12500):
+    imm = images[i]
+    ims = imm.split('/')[-1]
+    test_image = image.load_img(imm, target_size = (64, 64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result[i] = classifier.predict(test_image)
+    if result[i]==1:
+        dog[i]=1
+        cat[i]=0
+    else:
+        dog[i]=0
+        cat[i]=1
+    print('i = {} image = {} r = Dog:{} Cat:{}'.format(i,ims,dog[i],cat[i]))
+    imid['ID']=images
+    i = i+1
+
+sub['ID']=os.listdir('/home/yi-hsuancheng/Desktop/DogCat/test2/test/')
+sub['DOG']=dog
+sub['CAT']=cat 
+sub.head()
+sub.to_csv('sub.csv',index = False)
+#sub.to_csv('sub1.csv',index = True)
+
+#Import plot funtion 
+def append_history(history, h):
+     '''
+	This function appends the statistics over epochs
+     '''
+     try:
+       history.history['loss'] = history.history['loss'] + h.history['loss']
+       history.history['val_loss'] = history.history['val_loss'] + h.history['val_loss']
+       history.history['acc'] = history.history['acc'] + h.history['acc']
+       history.history['val_acc'] = history.history['val_acc'] + h.history['val_acc']
+     except:
+       history = h
+                
+     return history
+            
+
+def unfreeze_layer_onwards(model, layer_name):
+    '''
+        This layer unfreezes all layers beyond layer_name
+    '''
+    trainable = False
+    for layer in model.layers:
+        try:
+            if layer.name == layer_name:
+                trainable = True
+            layer.trainable = trainable
+        except:
+            continue
+    
+    return model
+            
+
+def plot_performance(history):
+    '''
+	This function plots the train & test accuracy, loss plots
+    '''
+        
+    plt.subplot(1,2,1)
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Accuracy v/s Epochs')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['train', 'test'], loc='upper left') 
+
+    plt.subplot(1,2,2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Loss v/s Epochs')
+    plt.ylabel('M.S.E Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['train', 'test'], loc='upper left') 
+
+    plt.tight_layout()
+    plt.show()
+
+
+plot_performance(history)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
